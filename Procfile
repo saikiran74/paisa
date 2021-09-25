772cd5cedd57dc81813e9f5254404d01fe3a1cc2
saikiran74 <bobbilisaikiran1999@gmail.com>
2021-09-25 13:22:46 +0530

    heroku
    deploying with requirement Procfile runtime files commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn paisa.wsgi --log-file -
--- paisa/settings.py
+++ paisa/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^4yuysz3f!#)@_8yd-j6)+km4yn33xlhj$t(l59h6m!-uo($u7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['paymemoney.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
