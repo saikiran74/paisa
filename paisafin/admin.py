e8de3a04e90b831815da87287ccea334009e690f
saikiran74 <78273124+saikiran74@users.noreply.github.com>
2021-09-27 22:41:44 +0530

    admin file to all tables

--- paisafin/admin.py
+++ paisafin/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Destination
+from .models import Destination,Country,Comments,Comments,Type,Language,Contactus,Silverbazar,All
 
 # Register your models here.
 
 admin.site.register(Destination)
+admin.site.register(Country)
+admin.site.register(Type)
+admin.site.register(Language)
+admin.site.register(All)
+admin.site.register(Contactus)
+admin.site.register(Silverbazar)
+admin.site.register(Comments)
