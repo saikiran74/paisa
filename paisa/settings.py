3d9af4258905b91256143baee44a399770f54eed
saikiran74 <78273124+saikiran74@users.noreply.github.com>
2021-09-27 09:56:11 +0530

    version7

--- paisa/settings.py
+++ paisa/settings.py
@@ -94,6 +94,11 @@ DATABASES = {
 }
 
 
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from-env)
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
